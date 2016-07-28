9b433f6878f5046775469af7affa40bb95b40c50
dschwartz783 <dschwartz783@gmail.com>
2016-07-28 03:08:30 -0400

    remove code no longer needed

--- src/DDSPlugins/PMSocial/FriendListDataProvider.php
+++ src/DDSPlugins/PMSocial/FriendListDataProvider.php
@@ -10,18 +10,7 @@ namespace DDSPlugins\PMSocial;
 
 class FriendListDataProvider extends DataProvider
 {
-    /** @var PMSocial $plugin */
-    protected $plugin;
-
-    /** @var string $friend_json_path*/
-    protected $friend_json_path = "hi";
-
-    /** @var array $friend_list */
-    protected $friend_list;
-
     function __construct(PMSocial $plugin) {
         parent::__construct($plugin, "friends.json");
-        $this->friend_list = $this->list;
-        $this->friend_json_path = $this->json_path;
     }
 }
